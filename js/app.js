a0c24f4bb14e996eb477682c94446f2de62adde2
MuhammadAtoum <muhammadatoum@gmail.com>
2021-03-16 21:23:21 +0200

    lab03

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+alert('welcome to my page');
+
+
+let name = prompt('what is your name ?');
+alert('will done thank you'+' '+ name);
+console.log('user Name', name);
+
+
+
+let start  = prompt('does muhammad visited petra ??');
+alert('Nice '+' '+ name);
+console.log('visit',start.toLowerCase());
+
+
+
+let age = prompt('is my age 26??');
+alert('keep going'+' '+ name);
+console.log('the age',age.toLowerCase());
+
+
+
+let work = prompt('does i worked for the Save the Children organization?');
+alert('Good job two questions left'+' '+name);
+console.log('SOjob',work.toLowerCase());
+
+
+
+let hope = prompt('does muhammad  love porograming');
+alert('Press OK to contnue');
+console.log('like porograming',hope.toLowerCase());
+
+
+
+
+let animal = prompt('does muhammad love catS??');
+alert('will done'+' '+ name);
+console.log('love pets',animal.toUpperCase());
+
+
+
